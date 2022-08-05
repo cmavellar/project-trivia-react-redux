1bfe8927185381980c73895c8597b38c37706413
leovcampos <leonardoviniciuscampos@gmail.com>
2022-08-05 17:48:39 -0300

    requisito 03

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Settings extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+  render() {
+    return (
+      <div
+        data-testid="settings-title"
+      >
+        Settings
+      </div>
+    );
+  }
+}
+
+export default Settings;
