b089a0983173df18a3969ac0f13a5c54b8e57b20
carolcortes <caroline.ocortes@gmail.com>
2022-08-09 19:00:43 -0300

    requisito 8

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -11,11 +11,34 @@ class Game extends Component {
       allAnswers: [],
       correct: 'correct',
       incorrect: 'wrong',
+      timer: 30,
+      disableBtn: false,
     };
   }
 
   componentDidMount() {
     this.fetchTriviaAPI();
+  }
+
+  // componentDidUpdate(_prevProps, prevState) {
+  //   this.clearTimer(prevState);
+  // }
+
+  clearTimer = (prevState) => {
+    if (prevState.timer === 0) {
+      clearInterval(this.timeInterval);
+      this.setState({
+        disableBtn: true,
+        timer: 0,
+      });
+    }
+  }
+
+  runQuestionTimer = () => {
+    const oneSecond = 1000;
+    this.timeInterval = setInterval(() => this.setState((prevState) => ({
+      timer: prevState.timer - 1,
+    })), oneSecond);
   }
 
   fetchTriviaAPI = async () => {
@@ -49,11 +72,12 @@ class Game extends Component {
   }
 
   render() {
-    const { questions, allAnswers, correct, incorrect } = this.state;
+    const { questions, allAnswers, correct, incorrect, timer, disableBtn } = this.state;
     console.log(questions);
     return (
       <>
         <Header />
+        <p>{ timer }</p>
         { questions.length > 0 && (
           <div>
             <p data-testid="question-category">{ questions[0].category }</p>
@@ -68,6 +92,7 @@ class Game extends Component {
               className={ answer === questions[0].correct_answer ? correct : incorrect }
               key={ index }
               onClick={ this.answerBtnClick }
+              disable={ disableBtn }
             >
               { answer }
             </button>
