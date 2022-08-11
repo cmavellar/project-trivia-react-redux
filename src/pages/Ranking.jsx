79fe3560858e1720b5de9d4387cb8b9c145ef534
Cássia Avellar <c.m.avellar@gmail.com>
2022-08-10 22:28:29 -0300

    Requisito 18 realizado

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
 //   constructor(props) {
 //     super(props);
 //   }
-  render() {
-    return (
+routeLogin = () => {
+  const { history } = this.props;
+  history.push('/');
+}
+
+render() {
+  return (
+    <div>
       <h1
         data-testid="ranking-title"
       >
         Ranking
       </h1>
-    );
-  }
+      <button
+        data-testid="btn-go-home"
+        type="button"
+        onClick={ this.routeLogin }
+      >
+        Voltar para o início
+      </button>
+    </div>
+  );
 }
+}
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+}.isRequired;
 
 export default Ranking;
